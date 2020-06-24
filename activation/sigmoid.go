437c6da6b368eee96198a172632c39ccc0f7b332
Cyril Dever <cdever@edgewhere.fr>
2020-06-24 18:29:59 +0200

    sigmoidPrime

--- activation/sigmoid.go
+++ activation/sigmoid.go
@@ -8,3 +8,8 @@ import (
 func Sigmoid(i, j int, z float64) float64 {
 	return 1 / (1 + math.Exp(-1*z))
 }
+
+// SigmoidPrime returns the derivative of the sigmoid function.
+func SigmoidPrime(i, j int, z float64) float64 {
+	return Sigmoid(i, j, z) * (1 - Sigmoid(i, j, z))
+}
